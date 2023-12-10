0ea134d2d79eabcaa328c77674f4d1d98a21a552
pvzchekmeneva20 <132128775+pvzchekmeneva20@users.noreply.github.com>
2023-12-10 16:40:58 +0300

    Add files via upload
    // Функция для удаления элементов с классом "wavebreaker__guide"
    // Функция для обработки нажатия на переключатель "Раскладка вещей"
    // Перемещение блока кода

---
+++ Remove Wavebreaker Guide-1.0.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Remove Wavebreaker Guide
+// @namespace    your-namespace
+// @version      1.0
+// @description  Removes elements with class "wavebreaker__guide"
+// @author       Vysokostnyi
+// @match        http://localhost:1100/pvz/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Функция для удаления элементов с классом "wavebreaker__guide"
+    function removeWavebreakerGuide() {
+        var elements = document.getElementsByClassName('wavebreaker__guide');
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].remove();
+        }
+    }
+
+    // Функция для обработки нажатия на переключатель "Раскладка вещей"
+    function handleSwitcherClick(event) {
+        var target = event.target;
+        var switcher = target.closest('.acceptance__switcher div');
+        if (switcher && switcher.classList.contains('active')) {
+            removeWavebreakerGuide();
+
+            // Перемещение блока кода
+            const originalElement = document.querySelector('acceptance-wavebreaker-boxes');
+            const destinationElement = document.querySelector('.acceptance__switcher');
+
+            destinationElement.insertAdjacentElement('afterend', originalElement);
+        }
+    }
+
+    // Обработчик события клика на переключатель
+    document.addEventListener('click', handleSwitcherClick);
+})();
