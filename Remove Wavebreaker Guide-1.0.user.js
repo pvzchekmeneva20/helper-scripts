abb12dbb4dece3729d0a2252d8777272c450a6a7
pvzchekmeneva20 <132128775+pvzchekmeneva20@users.noreply.github.com>
2023-12-21 16:49:05 +0300

    Update Remove Wavebreaker Guide.user.js
    Улучшенная версия

--- Remove Wavebreaker Guide-1.0.user.js
+++ Remove Wavebreaker Guide-1.0.user.js
@@ -1,44 +1,47 @@
 // ==UserScript==
 // @name         Remove Wavebreaker Guide
 // @namespace    your-namespace
-// @version      1.1
+// @version      1.3
 // @description  Removes elements with class "wavebreaker__guide"
 // @author       Vysokostnyi
-// @match        http://*/pvz/acceptance*
+// @match        http://*/pvz/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    'use strict';
+  'use strict';
 
-    // Функция для удаления элементов с классом "wavebreaker__guide"
-    function removeWavebreakerGuide() {
-        var elements = document.getElementsByClassName('wavebreaker__guide');
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].remove();
-        }
-    }
-
-    // Функция для обработки нажатия на переключатель "Раскладка вещей"
-    function handleSwitcherClick(event) {
-        var target = event.target;
-        var switcher = target.closest('.acceptance__switcher div');
-        if (switcher && switcher.classList.contains('active')) {
-            removeWavebreakerGuide();
-
-            // Перемещение блока кода
-            const originalElement = document.querySelector('acceptance-wavebreaker-boxes');
-            const destinationElement = document.querySelector('.acceptance__switcher');
-
-            destinationElement.insertAdjacentElement('afterend', originalElement);
-        }
-    }
-
-    // Обработчик события клика на переключатель
-    document.addEventListener('click', handleSwitcherClick);
-
-    // Обработчик изменений в дереве DOM
-    document.addEventListener("DOMSubtreeModified", function() {
-        removeWavebreakerGuide();
+  function removeWavebreakerGuide() {
+    let elements = document.querySelectorAll('.wavebreaker__guide');
+    Array.from(elements).forEach(element => {
+      element.remove();
     });
+  }
+
+  function moveWavebreakerBoxes() {
+    let existingElement = document.querySelector('acceptance-wavebreaker-boxes.moved');
+    let newElement = document.querySelector('acceptance-wavebreaker-boxes:not(.moved)');
+
+    if (newElement) {
+      if (existingElement) {
+        existingElement.remove();
+      }
+
+      const destinationElement = document.querySelector('.acceptance__switcher');
+      destinationElement.insertAdjacentElement('afterend', newElement);
+      newElement.classList.add('moved');
+    }
+  }
+
+  document.addEventListener('click', function(event) {
+    if (event.target.closest('.acceptance__switcher div.active')) {
+      removeWavebreakerGuide();
+    }
+  });
+
+  let observer = new MutationObserver(function() {
+    removeWavebreakerGuide();
+    setTimeout(moveWavebreakerBoxes, 100);
+  });
+  observer.observe(document, { subtree: true, childList: true });
 })();
